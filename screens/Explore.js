bfc1931f2c88b7486687815eee1737180ebc821d
Rubin Bajracharya <rubin@fusemachines.com>
2018-06-07 22:07:38 +0545

    Completed Upto Animated Header View

--- screens/Explore.js
+++ screens/Explore.js
@@ -9,28 +9,52 @@ import {
   StatusBar,
   ScrollView,
   Image,
-  Dimensions
+  Dimensions,
+  Animated
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import Category from './components/Explore/Category'
+import Home from './components/Explore/Home'
+import Tag from './components/Explore/Tag'
 
 const { width, height } = Dimensions.get('window')
 
 class Explore extends Component {
   componentWillMount() {
+    this.scrollY = new Animated.Value(0)
+
     this.startHeaderHeight = 80
+    this.endHeaderHeight = 50
+
     if (Platform.OS == 'android') {
       this.startHeaderHeight = 100 + StatusBar.currentHeight
+      this.endHeaderHeight = 70 + StatusBar.currentHeight
     }
+
+    this.animatedHeaderHeight = this.scrollY.interpolate({
+      inputRange: [0, 50],
+      outputRange: [this.startHeaderHeight, this.endHeaderHeight],
+      extrapolate: 'clamp'
+    })
+    this.animatedOpacity = this.animatedHeaderHeight.interpolate({
+      inputRange: [this.endHeaderHeight, this.startHeaderHeight],
+      outputRange: [0, 1],
+      extrapolate: 'clamp'
+    })
+    this.animatedTagTop = this.animatedHeaderHeight.interpolate({
+      inputRange: [this.endHeaderHeight, this.startHeaderHeight],
+      outputRange: [-30, 10],
+      extrapolate: 'clamp'
+    })
   }
 
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <View style={{ flex: 1 }}>
-          <View
+          <Animated.View
             style={{
-              height: this.startHeaderHeight,
+              height: this.animatedHeaderHeight,
               backgroundColor: 'white',
               borderBottomWidth: 1,
               borderBottomColor: '#dddddd'
@@ -49,16 +73,44 @@ class Explore extends Component {
                 marginTop: Platform.OS == 'android' ? 30 : null
               }}
             >
-              <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
+              <Icon
+                name="ios-search"
+                size={20}
+                style={{ marginRight: 10, marginTop: 5, marginLeft: 5 }}
+              />
               <TextInput
                 underlineColorAndroid="transparent"
                 placeholder="Try New Delhi"
                 placeholderTextColor="grey"
-                style={{ flex: 1, fontWeight: '700', backgroundColor: 'white' }}
+                style={{
+                  flex: 1,
+                  fontWeight: '500',
+                  backgroundColor: 'white',
+                  fontSize: 16
+                }}
               />
             </View>
-          </View>
-          <ScrollView scrollEventThrottle={16}>
+            <Animated.View
+              style={{
+                flexDirection: 'row',
+                marginHorizontal: 20,
+                position: 'relative',
+                top: this.animatedTagTop,
+                paddingBottom: 20,
+                opacity: this.animatedOpacity
+              }}
+            >
+              <Tag name="Guests" />
+
+              <Tag name="Dates" />
+            </Animated.View>
+          </Animated.View>
+          <ScrollView
+            scrollEventThrottle={16}
+            onScroll={Animated.event([
+              { nativeEvent: { contentOffset: { y: this.scrollY } } }
+            ])}
+          >
             <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20 }}>
               <Text
                 style={{
@@ -87,6 +139,18 @@ class Explore extends Component {
                     imageUrl={require('../assets/experiences.jpg')}
                     name="Experience"
                   />
+                  <Category
+                    imageUrl={require('../assets/home.jpg')}
+                    name="Home"
+                  />
+                  <Category
+                    imageUrl={require('../assets/restaurant.jpg')}
+                    name="Restaurant"
+                  />
+                  <Category
+                    imageUrl={require('../assets/experiences.jpg')}
+                    name="Experience"
+                  />
                 </ScrollView>
               </View>
 
@@ -99,8 +163,61 @@ class Explore extends Component {
                 </Text>
 
                 <View style={{ width: width - 40, height: 200, marginTop: 20 }}>
-                  <Image 
-                  source={require('../assets/home.jpg')} style={{flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 5, borderWidth: 1, borderColor: '#dddddd'}}/>
+                  <Image
+                    source={require('../assets/home.jpg')}
+                    style={{
+                      flex: 1,
+                      height: null,
+                      width: null,
+                      resizeMode: 'cover',
+                      borderRadius: 5,
+                      borderWidth: 1,
+                      borderColor: '#dddddd'
+                    }}
+                  />
+                </View>
+              </View>
+
+              <View style={{ marginTop: 40 }}>
+                <Text
+                  style={{
+                    fontSize: 24,
+                    fontWeight: '700',
+                    paddingHorizontal: 20
+                  }}
+                >
+                  Homes around the world
+                </Text>
+                <View
+                  style={{
+                    paddingHorizontal: 20,
+                    marginTop: 20,
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <Home
+                    width={width}
+                    name="The Cozy Place"
+                    type="PRIVATE ROOM - 2 BEDS"
+                    price={82}
+                    rating={4}
+                  />
+                  <Home
+                    width={width}
+                    name="The Cozy Place"
+                    type="PRIVATE ROOM - 2 BEDS"
+                    price={42}
+                    rating={4.5}
+                  />
+                  <Home
+                    width={width}
+                    name="The Cozy Place"
+                    type="PRIVATE ROOM - 2 BEDS"
+                    price={54}
+                    rating={3}
+                  />
                 </View>
               </View>
             </View>
@@ -111,11 +228,3 @@ class Explore extends Component {
   }
 }
 export default Explore
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
